ab7e998797a1e5aba6b06657b32fb63ab58fb3e7
gitName <bartk@list.ru>
2025-01-19 17:00:30 +0300

    Revert "refactor: add spaces into the code for better readability"
    This reverts commit 787a3f2cbd339f3bcf7ab8a72638657da988321e.

--- public/js/components/task.js
+++ public/js/components/task.js
@@ -1,13 +1,13 @@
 export default {
   view(data) {
-    const { id, is_completed, task } = data;
+    const {id, is_completed, task} = data;
     return `
         <li class="list-group-item shadow m-2">
-            <span class="task ${is_completed ? "completed" : ""}">${task}</span>
+            <span class="${is_completed ? "completed" : ""}">${task}</span>
             <button class="btn btn-primary rounded editTaskBtn" data-id="${id}">Edit</button>
             <button class="btn btn-success rounded toggleTaskBtn" data-id="${id}">${
-      is_completed ? "To do again" : "Done"
-    }</button>
+                is_completed ? "To do again" : "Done"
+                }</button>
             <button class="btn btn-danger rounded deleteTaskBtn" data-id="${id}">Delete</button>
         </li>
     `;
